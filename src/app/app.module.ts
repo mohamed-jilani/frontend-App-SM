27920bfb3abb79a57ca33b4ae7786c941a6ac664
jilan <mohamedjilani551@gmail.com>
2024-03-19 21:41:38 +0100

    le 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { BoardModeratorComponent } from './pages/board-moderator/board-moderator.component';
+import { BoardAdminComponent } from './pages/board-admin/board-admin.component';
+import { BoardUserComponent } from './pages/board-user/board-user.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    BoardModeratorComponent,
+    BoardAdminComponent,
+    BoardUserComponent,
+    HomeComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
